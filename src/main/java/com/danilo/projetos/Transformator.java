b0918ae980ee3e66ab8ea0880625c7deb89a7ca7
danilovalerio <danilo_vs@hotmail.com>
2024-09-27 15:57:07 -0300

    tipo output

--- src/main/java/com/danilo/projetos/Transformator.java
+++ src/main/java/com/danilo/projetos/Transformator.java
@@ -4,12 +4,16 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Transformator {
 
-    public <I, O> transformar(I input) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+    public <I, O> O transformar(I input) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+            , NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+        //Classe que vai ser transformada
         Class<?> source = input.getClass();
         Class<?> target = source.forName(source.getClass() + "DTO");
 
         //busca os construtores em tempo de execuçao
         O targetClass = (O) target.getDeclaredConstructor().newInstance();
+
+        return targetClass;
 
     }
 }
