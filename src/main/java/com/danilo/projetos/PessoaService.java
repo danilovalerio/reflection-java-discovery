d2a71212a9e56842ef180a6cfb5f93f6955acd46
danilovalerio <danilo_vs@hotmail.com>
2024-09-27 16:19:15 -0300

    Usando o Transformator

--- src/main/java/com/danilo/projetos/PessoaService.java
+++ src/main/java/com/danilo/projetos/PessoaService.java
@@ -1,10 +1,15 @@
 package com.danilo.projetos;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class PessoaService {
 
-    public PessoaDTO list() {
+    public PessoaDTO list() throws ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
         Pessoa pessoa = new PessoaRepository().list();
-        PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+
+        PessoaDTO pessoaDTO =new Transformator().transformar(pessoa);
+
+        //PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
         return pessoaDTO;
     }
 }
